0a0d45bbf33eea3da425e154d5e76e78a8cd45be
Nick Ashford <nick1973_5@btinternet.com>
2016-10-12 10:27:11 +0100

    pre-book shows all days

--- app/Http/Controllers/PreBookController.php
+++ app/Http/Controllers/PreBookController.php
@@ -49,7 +49,7 @@ class PreBookController extends Controller
         }
         $input = $request->all();
         Tracking::create($input);
-        return redirect('pre-booking')->with('status', 'Ticket\'s Been Booked!');
+        return redirect('home')->with('status', 'Ticket\'s Been Booked!');
     }
 
     public function issueTicket($id)
