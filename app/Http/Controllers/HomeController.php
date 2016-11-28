612b5ca55c81d29dd601c357ec0844fb839bdc4d
Nick Ashford <nick1973_5@btinternet.com>
2016-11-28 09:39:19 +0000

    ticket fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -99,7 +99,7 @@ class HomeController extends Controller
             $ticket_serial_number = 'valet3_ticket_serial_number';
         }
 
-        $lastRecord = Tracking::latest('id')->where($ticket_id, '!=', '')->where('ticket_status', '=', 'active')
+        $lastRecord = Tracking::latest($ticket_id)->where($ticket_id, '!=', '')->where('ticket_status', '=', 'active')
             ->orWhere('ticket_status', '=', 'complete')->first();
 
         return $lastRecord;
